a3de4814ab706d385dd68fa00d1d429283c45699
nikola-velemir <vnexa021@gmail.com>
2025-10-17 15:24:01 +0200

    [feature/wip] ipynb - statistics

--- src/generator/statistics/split_statistics.py
+++ src/generator/statistics/split_statistics.py
@@ -3,9 +3,8 @@ import pandas as pd
 from src.generator.statistics.statistics_utils import run_statistics
 
 
-# Load the dataset
 if __name__ == "__main__":
 
 
-    df = pd.read_csv('results/test/test_data_1760701926.5381536.csv')
+    df = pd.read_csv('results/test/test_data_1760707261.963145.csv')
     run_statistics(df)
